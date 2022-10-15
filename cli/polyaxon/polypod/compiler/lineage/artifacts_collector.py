17204a64c29fe71904b7925a806551058895b4b4
polyaxon-ci <ci@polyaxon.com>
2022-10-15 18:15:49 +0200

    Lint

--- cli/polyaxon/polypod/compiler/lineage/artifacts_collector.py
+++ cli/polyaxon/polypod/compiler/lineage/artifacts_collector.py
@@ -22,7 +22,9 @@ from traceml.artifacts import V1ArtifactKind, V1RunArtifact
 
 
 def collect_lineage_artifacts_path(artifact_path: str) -> Optional[V1RunArtifact]:
-    name = os.path.basename(artifact_path.rstrip("/"))  # Trim handles cases like `foo/` -> ''
+    name = os.path.basename(
+        artifact_path.rstrip("/")
+    )  # Trim handles cases like `foo/` -> ''
     return V1RunArtifact(
         name=to_fqn_name(name) if name else "_",
         kind=V1ArtifactKind.DIR,
