1f691bc7dbe30aa21931aa95e23664d3711a5739
polyaxon-ci <ci@polyaxon.com>
2023-03-31 11:47:22 +0200

    Improve serialization/deserialization to handle null values

--- cli/polyaxon/utils/serialization.py
+++ cli/polyaxon/utils/serialization.py
@@ -21,11 +21,11 @@ from pydantic.validators import uuid_validator
 date_serialize = lambda x: x.isoformat()
 date_deserialize = lambda x: parse_date(x)
 
-datetime_serialize = lambda x: x.isoformat()
-datetime_deserialize = lambda x: parse_datetime(x)
+datetime_serialize = lambda x: x.isoformat() if x else x
+datetime_deserialize = lambda x: parse_datetime(x) if x else x
 
-timedelta_serialize = lambda x: x.total_seconds()
-timedelta_deserialize = lambda x: parse_duration(x)
+timedelta_serialize = lambda x: x.total_seconds() if x else x
+timedelta_deserialize = lambda x: parse_duration(x) if x else x
 
 
 class _DummyField:
@@ -34,5 +34,5 @@ class _DummyField:
 
 _dummy_field: Any = _DummyField()
 
-uuid_serialize = lambda x: x.hex
-uuid_deserialize = lambda x: uuid_validator(x, _dummy_field)
+uuid_serialize = lambda x: x.hex if x else x
+uuid_deserialize = lambda x: uuid_validator(x, _dummy_field) if x else x
