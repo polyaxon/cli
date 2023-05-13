b8051d3e8a776502c43b6203c07a8c3a9e3c67f1
polyaxon-ci <ci@polyaxon.com>
2023-05-13 12:28:19 +0200

    Remove unnecessary headers

--- cli/polyaxon/sdk/api/__init__.py
+++ cli/polyaxon/sdk/api/__init__.py
@@ -1,19 +1,3 @@
-#!/usr/bin/python
-#
-# Copyright 2018-2023 Polyaxon, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from polyaxon.sdk.api.agents_v1_api import AgentsV1Api
 from polyaxon.sdk.api.artifacts_stores_v1_api import ArtifactsStoresV1Api
 from polyaxon.sdk.api.auth_v1_api import AuthV1Api
