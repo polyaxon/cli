49ed110ec222db712b0841524cb0071c68e77693
polyaxon-ci <ci@polyaxon.com>
2023-06-26 19:50:03 +0200

    Fix viewer logic for offline paths

--- cli/polyaxon/fs/utils.py
+++ cli/polyaxon/fs/utils.py
@@ -2,18 +2,16 @@ import os
 
 from typing import Optional
 
+from polyaxon.env_vars.keys import EV_KEYS_SANDBOX_IS_LOCAL
 from polyaxon.lifecycle import V1ProjectFeature
 
 
 def get_store_path(store_path: str, subpath: str, entity: Optional[str] = None) -> str:
     full_path = store_path
-    if entity == V1ProjectFeature.RUNTIME:
-        from polyaxon.services.values import PolyaxonServices
 
-        if PolyaxonServices.is_sandbox():
-            full_path = os.path.join(full_path, "runs")
-    else:
-        full_path = os.path.join(full_path, f"{entity}s")
+    if os.environ.get(EV_KEYS_SANDBOX_IS_LOCAL):
+        dir_path = "runs" if entity == V1ProjectFeature.RUNTIME else f"{entity}s"
+        full_path = os.path.join(full_path, dir_path)
 
     if subpath:
         full_path = os.path.join(full_path, subpath)
