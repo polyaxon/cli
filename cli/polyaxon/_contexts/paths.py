6c03cb415660249be3d95d85064cdf65d6e4bd8d
polyaxon-ci <ci@polyaxon.com>
2023-10-24 22:26:36 +0200

    Improve mount sandbox logic

--- cli/polyaxon/_contexts/paths.py
+++ cli/polyaxon/_contexts/paths.py
@@ -53,7 +53,6 @@ CONTEXT_TMP_RUNS_ROOT_FORMAT = os.environ.get(
 )
 CONTEXT_ARCHIVES_ROOT = os.environ.get(ENV_KEYS_ARCHIVES_ROOT, "/tmp/plx/archives")
 CONTEXT_ARTIFACTS_ROOT = os.environ.get(ENV_KEYS_ARTIFACTS_ROOT, "/tmp/plx/artifacts")
-CONTEXT_SANDBOX_ROOT = None
 CONTEXT_OFFLINE_ROOT = os.environ.get(ENV_KEYS_OFFLINE_ROOT, "/tmp/plx/offline")
 CONTEXT_OFFLINE_FORMAT = "{}/{{}}".format(CONTEXT_OFFLINE_ROOT)
 CONTEXT_ARTIFACTS_FORMAT = "{}/{{}}".format(CONTEXT_ARTIFACTS_ROOT)
@@ -84,15 +83,14 @@ def get_offline_path(
 
 
 def mount_sandbox(path: Optional[str] = None):
-    global CONTEXT_SANDBOX_ROOT
     global CONTEXT_OFFLINE_ROOT
     global CONTEXT_ARTIFACTS_ROOT
     global CONTEXT_OFFLINE_FORMAT
     global CONTEXT_ARTIFACTS_FORMAT
 
     path = path or CONTEXT_OFFLINE_ROOT
-    CONTEXT_SANDBOX_ROOT = os.environ.get(ENV_KEYS_SANDBOX_ROOT, path)
-    CONTEXT_OFFLINE_ROOT = CONTEXT_SANDBOX_ROOT
-    CONTEXT_ARTIFACTS_ROOT = CONTEXT_SANDBOX_ROOT
+    CONTEXT_OFFLINE_ROOT = path
+    CONTEXT_ARTIFACTS_ROOT = path
     CONTEXT_OFFLINE_FORMAT = "{}/{{}}".format(CONTEXT_OFFLINE_ROOT)
     CONTEXT_ARTIFACTS_FORMAT = "{}/{{}}".format(CONTEXT_ARTIFACTS_ROOT)
+    return path
