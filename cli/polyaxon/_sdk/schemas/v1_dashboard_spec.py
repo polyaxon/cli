16395b21a48ed87ff13bc544f68f24bf357aa7a9
polyaxon-ci <ci@polyaxon.com>
2024-03-28 11:14:06 +0100

    Update sections and dashboard specs with settings

--- cli/polyaxon/_sdk/schemas/v1_dashboard_spec.py
+++ cli/polyaxon/_sdk/schemas/v1_dashboard_spec.py
@@ -6,4 +6,8 @@ from polyaxon._sdk.schemas.v1_section_spec import V1SectionSpec
 
 
 class V1DashboardSpec(BaseAllowSchemaModel):
+    xaxis: Optional[str]
+    smoothing: Optional[int]
+    ignore_outliers: Optional[bool]
+    sample_size: Optional[int]
     sections: Optional[List[V1SectionSpec]]
--- cli/polyaxon/_sdk/schemas/v1_section_spec.py
+++ cli/polyaxon/_sdk/schemas/v1_section_spec.py
@@ -7,8 +7,13 @@ from clipped.config.schema import BaseAllowSchemaModel
 class V1SectionSpec(BaseAllowSchemaModel):
     name: Optional[StrictStr]
     is_minimized: Optional[bool]
+    is_frozen: Optional[str]
     columns: Optional[int]
     height: Optional[int]
+    xaxis: Optional[str]
+    smoothing: Optional[int]
+    ignore_outliers: Optional[bool]
+    sample_size: Optional[int]
     widgets: Optional[List[Dict[str, Any]]]
-    page_index: Optional[int] = Field(alias="pageIndex")
-    page_size: Optional[int] = Field(alias="pageSize")
+    page_index: Optional[int]
+    page_size: Optional[int]
