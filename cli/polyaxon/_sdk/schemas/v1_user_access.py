9f358bb5a377d3fc7e3d541fb4ce5a67320db67f
polyaxon-ci <ci@polyaxon.com>
2024-08-08 12:22:15 +0200

    Update user access to all service accounts

--- cli/polyaxon/_sdk/schemas/v1_user_access.py
+++ cli/polyaxon/_sdk/schemas/v1_user_access.py
@@ -4,8 +4,14 @@ from clipped.compact.pydantic import StrictStr
 from clipped.config.schema import BaseAllowSchemaModel
 
 
+class V1UserAccessData(BaseAllowSchemaModel):
+    username: Optional[StrictStr]
+    is_sa: Optional[bool]
+
+
 class V1UserAccess(BaseAllowSchemaModel):
     user: Optional[StrictStr]
+    user_data: Optional[V1UserAccessData]
     queue: Optional[StrictStr]
     preset: Optional[StrictStr]
     namespace: Optional[StrictStr]
