4079d0839dac44899dff340e346cc3ad492e6ffa
polyaxon-ci <ci@polyaxon.com>
2025-04-21 22:28:45 +0200

    Bump version to v2.8.0-rc2

--- cli/polyaxon/pkg.py
+++ cli/polyaxon/pkg.py
@@ -1,5 +1,5 @@
 NAME = "polyaxon"
-VERSION = "2.7.0"
+VERSION = "2.8.0-rc2"
 SCHEMA_VERSION = 1.1
 DESC = "Command Line Interface (CLI) and client to interact with Polyaxon API."
 URL = "https://github.com/polyaxon/polyaxon"
