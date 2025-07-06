100a08897962e7ba2fd3c8344ea03c4166c3bc05
polyaxon-ci <ci@polyaxon.com>
2025-07-06 12:55:00 +0100

    Bump version to v2.8.1-rc2

--- cli/polyaxon/_deploy/schemas/service.py
+++ cli/polyaxon/_deploy/schemas/service.py
@@ -30,7 +30,8 @@ class AgentServiceConfig(DeploymentService):
     service_account_annotations: Optional[Dict] = Field(
         alias="serviceAccountAnnotations", default=None
     )
-    enable_finalizers: Optional[bool] = Field(alias="enableFinalizers", default=None)
+    enable_status_finalizers: Optional[bool] = Field(alias="enableStatusFinalizers", default=None)
+    enable_logs_finalizers: Optional[bool] = Field(alias="enableLogsFinalizers", default=None)
     is_replica: Optional[bool] = Field(alias="isReplica", default=None)
 
 
